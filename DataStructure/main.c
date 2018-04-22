b6235fce9443903f1ee5808de793c01cb32f2f87
LittleSakana <dsdevelopment@foxmail.com>
2018-04-22 23:48:34 +0800

    1. 计算最大子项和

--- DataStructure/main.c
+++ DataStructure/main.c
@@ -14,13 +14,26 @@ void printN (int n);
 clock_t start,stop;
 double duration;
 
+int getMaxSubSequence (int A[], int n);
+
 int main(int argc, const char * argv[]) {
-    
-    start = clock();
-    printN(100000);
-    stop = clock();
-    duration = (double)(stop - start)/CLOCKS_PER_SEC;
-    printf("总共用时%.2f",duration);
+//    start = clock();
+//    printN(100000);
+//    stop = clock();
+//    duration = (double)(stop - start)/CLOCKS_PER_SEC;
+//    printf("总共用时%.2f",duration);
+
+    int n;
+    if (scanf("%d", &n) == 1) {
+        int input[n];
+        for (int i = 0; i < n; i++) {
+            if (scanf("%d",&input[i]) == 1) {
+                
+            }
+        }
+        int max = getMaxSubSequence(input, n);
+        printf("%d",max);
+    }
     return 0;
 }
 
@@ -29,4 +42,19 @@ void printN (int n) {
     for (i = 0; i <= n; i++) {
         printf("%d\n",i);
     }
+}
+
+// 计算最大子项值
+int getMaxSubSequence (int A[], int n) {
+    int maxSum,currentSum;
+    maxSum = currentSum = 0;
+    for (int i = 0; i < n; i++) {
+        currentSum += A[i];
+        if (currentSum > maxSum) {
+            maxSum = currentSum;
+        } else if (currentSum < 0) {
+            currentSum = 0;
+        }
+    }
+    return maxSum;
 }
